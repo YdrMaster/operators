9f6f2452142339f68faeb8c5b67a94eaa5e60c98
kilinchange <kilinchange@163.com>
2024-06-04 15:25:12 +0800

    feat(kernel):添加 rotary_embedding cuda 算子

--- src/optype.h
+++ src/optype.h
@@ -16,7 +16,7 @@ struct Kernel;
 
 typedef void (*RmsNormFn)(struct Kernel const *, MutTensor y, ConstTensor x, ConstTensor w, float epsilon);
 typedef void (*MatMulFn)(struct Kernel const *, MutTensor c, float beta, ConstTensor a, ConstTensor b, float alpha);
-typedef void (*RotaryEmbeddingFn)(struct Kernel const *, MutTensor, ConstTensor pos, float theta);
+typedef void (*RotaryEmbeddingFn)(struct Kernel const *, MutTensor t, ConstTensor pos, float theta);
 typedef void (*ReformFn)(struct Kernel const *, MutTensor dst, ConstTensor src);
 typedef void (*CausalSoftmaxFn)(struct Kernel const *, MutTensor);
 typedef void (*SwigluFn)(struct Kernel const *, MutTensor, ConstTensor);
