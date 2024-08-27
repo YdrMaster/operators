5d52c85064aa6b8d3df507479ce06d741c37b3c0
kilinchange <kilinchange@163.com>
2024-08-27 17:10:18 +0800

    refactor: refactor swiglu cpu&cuda

--- include/ops/swiglu/swiglu.h
+++ include/ops/swiglu/swiglu.h
@@ -4,14 +4,31 @@
 #include "../../export.h"
 #include "../../operators.h"
 
-typedef struct SwigluDescriptor SwigluDescriptor;
-typedef SwigluDescriptor* infiniopSwiGLUDescriptor_t;
+typedef struct SwiGLUDescriptor {
+    Device device;
+} SwiGLUDescriptor;
 
-// @deprecated
-__C __export void *createSwigluDescriptor(Device, void *config);
-// @deprecated
-__C __export void destroySwigluDescriptor(SwigluDescriptor *descriptor);
-// @deprecated
-__C __export void swiglu(SwigluDescriptor *descriptor, Tensor gate, Tensor up, void *stream);
+typedef SwiGLUDescriptor *infiniopSwiGLUDescriptor_t;
+
+__C __export infiniopStatus_t infiniopCreateSwiGLUDescriptor(infiniopHandle_t handle,
+                                                             infiniopSwiGLUDescriptor_t *desc_ptr,
+                                                             infiniopTensorDescriptor_t c_desc,
+                                                             infiniopTensorDescriptor_t a_desc,
+                                                             infiniopTensorDescriptor_t b_desc);
+
+__C __export infiniopStatus_t infiniopSwiGLU(infiniopSwiGLUDescriptor_t desc,
+                                             void *c,
+                                             void *a,
+                                             void *b,
+                                             void *stream);
+
+__C __export infiniopStatus_t infiniopDestroySwiGLUDescriptor(infiniopSwiGLUDescriptor_t desc);
+
+// // @deprecated
+// __C __export void *createSwigluDescriptor(Device, void *config);
+// // @deprecated
+// __C __export void destroySwigluDescriptor(SwigluDescriptor *descriptor);
+// // @deprecated
+// __C __export void swiglu(SwigluDescriptor *descriptor, Tensor gate, Tensor up, void *stream);
 
 #endif
--- src/ops/swiglu/cpu/swiglu_cpu.h
+++ src/ops/swiglu/cpu/swiglu_cpu.h
@@ -3,10 +3,30 @@
 
 #include "operators.h"
 
-struct SwigluCpuDescriptor {
+struct SwiGLUCpuDescriptor {
     Device device;
+    DT dtype;
+    uint64_t seq_len;
+    uint64_t di;
+    uint64_t stride_a;
+    uint64_t stride_b;
+    uint64_t stride_c;
 };
 
-void swiglu_cpu_f16(Tensor gate, Tensor up);
+typedef struct SwiGLUCpuDescriptor *SwiGLUCpuDescriptor_t;
+
+infiniopStatus_t cpuCreateSwiGLUDescriptor(infiniopHandle_t handle,
+                                           SwiGLUCpuDescriptor_t *desc_ptr,
+                                           infiniopTensorDescriptor_t c_dec,
+                                           infiniopTensorDescriptor_t a_desc,
+                                           infiniopTensorDescriptor_t b_desc);
+
+infiniopStatus_t cpuSwiGLU(SwiGLUCpuDescriptor_t desc,
+                           void *c,
+                           void *a,
+                           void *b,
+                           void *stream);
+
+infiniopStatus_t cpuDestroySwiGLUDescriptor(SwiGLUCpuDescriptor_t desc);
 
 #endif// __CPU_SWIGLU_H__
