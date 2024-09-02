517d62cc4aafa34ac65487ccfa5e113a7bd745f6
kilinchange <kilinchange@163.com>
2024-09-02 17:48:36 +0800

    refactor: refactor matmul cpu and test

--- include/ops/matmul/matmul.h
+++ include/ops/matmul/matmul.h
@@ -4,14 +4,30 @@
 #include "../../export.h"
 #include "../../operators.h"
 
-typedef struct MatmulDescriptor MatmulDescriptor;
-typedef MatmulDescriptor* infiniopMatmulDescriptor_t;
+typedef struct MatmulDescriptor {
+    Device device;
+} MatmulDescriptor;
 
-// @deprecated
-__C __export MatmulDescriptor *createMatmulDescriptor(Device, void *config);
-// @deprecated
-__C __export void destroyMatmulDescriptor(MatmulDescriptor *descriptor);
-// @deprecated
-__C __export void matmul(MatmulDescriptor *descriptor, Tensor c, float beta, Tensor a, Tensor b, float alpha, void *stream);
+typedef MatmulDescriptor *infiniopMatmulDescriptor_t;
+
+__C __export infiniopStatus_t infiniopCreateMatmulDescriptor(infiniopHandle_t handle,
+                                                             infiniopMatmulDescriptor_t *desc_ptr,
+                                                             infiniopTensorDescriptor_t c_desc,
+                                                             infiniopTensorDescriptor_t a_desc,
+                                                             infiniopTensorDescriptor_t b_desc);
+
+__C __export infiniopStatus_t infiniopGetMatmulWorkspaceSize(infiniopMatmulDescriptor_t desc, uint64_t *size);
+
+__C __export infiniopStatus_t infiniopMatmul(infiniopMatmulDescriptor_t desc,
+                                             void *workspace,
+                                             uint64_t workspace_size,
+                                             void *c,
+                                             void *a,
+                                             void *b,
+                                             float alpha,
+                                             float beta,
+                                             void *stream);
+
+__C __export infiniopStatus_t infiniopDestroyMatmulDescriptor(infiniopMatmulDescriptor_t desc);
 
 #endif
