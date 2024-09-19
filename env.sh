d0d65abcc13bad63d0807ca74b2464ead0792a27
zhangyue <138768300+zhangyue207@users.noreply.github.com>
2024-09-19 13:54:33 +0800

    Delete env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-export ASCEND_HOME=/usr/local/Ascend/ascend-toolkit/latest
-export SOC_VERSION=Ascend910B3
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-source /usr/local/Ascend/toolbox/set_env.sh
