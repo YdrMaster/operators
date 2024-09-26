ac5a6fa01ad753e0abcffb26e3232f6f176f391e
zhangyue <14568307+zhangyue207@user.noreply.gitee.com>
2024-09-26 15:46:06 +0800

    fix handle

--- src/devices/ascend/ascend_handle.h
+++ src/devices/ascend/ascend_handle.h
@@ -2,9 +2,9 @@
 #define ASCEND_HANDLE_H
 
 #include "../pool.h"
+#include "common_ascend.h"
 #include "device.h"
 #include "status.h"
-#include "common_ascend.h"
 #include <acl/acl.h>
 #include <acl/acl_base.h>
 #include <acl/acl_rt.h>
@@ -26,6 +26,10 @@ template<typename T>
 void use_aclnn(AscendHandle_t handle, T const &f) {
     auto &pool = handle->aclnn_handles;
     auto executor = pool->pop();
+    if (!executor.has_value()) {
+        aclrtSetDevice(handle->device_id);
+        *executor = nullptr;
+    }
     f(*executor);
     pool->push(std::move(*executor));
 }
