f95b33f77196de403d1c1bdaa18805246a0a2aa3
panzezhong <panzezhong@qiyuanlab.com>
2024-10-17 10:48:41 +0800

    fix: 创建tensor descriptor时使用const形状和步长

--- include/tensor/tensor_descriptor.h
+++ include/tensor/tensor_descriptor.h
@@ -5,7 +5,7 @@
 #include "../tensor.h"
 #include "../status.h"
 
-__C __export infiniopStatus_t infiniopCreateTensorDescriptor(infiniopTensorDescriptor_t *desc_ptr, uint64_t ndim, uint64_t *shape_, int64_t *strides_, DataLayout datatype);
+__C __export infiniopStatus_t infiniopCreateTensorDescriptor(infiniopTensorDescriptor_t *desc_ptr, uint64_t ndim, uint64_t const *shape_, int64_t const *strides_, DataLayout datatype);
 
 __C __export infiniopStatus_t infiniopDestroyTensorDescriptor(infiniopTensorDescriptor_t desc);
 
--- src/tensor/tensor_descriptor.cc
+++ src/tensor/tensor_descriptor.cc
@@ -1,7 +1,7 @@
 #include "tensor/tensor_descriptor.h"
 #include <cstring>
 
-__C __export infiniopStatus_t infiniopCreateTensorDescriptor(infiniopTensorDescriptor_t *desc_ptr, uint64_t ndim, uint64_t *shape_, int64_t *strides_, DataLayout datatype) {
+__C __export infiniopStatus_t infiniopCreateTensorDescriptor(infiniopTensorDescriptor_t *desc_ptr, uint64_t ndim, uint64_t const *shape_, int64_t const *strides_, DataLayout datatype) {
     uint64_t *shape = new uint64_t[ndim];
     int64_t *strides = new int64_t[ndim];
     std::memcpy(shape, shape_, ndim * sizeof(uint64_t));
