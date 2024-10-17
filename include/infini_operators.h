7846a7031cf49852e00de93242185077059b2f7c
panzezhong <panzezhong@qiyuanlab.com>
2024-10-17 15:13:42 +0800

    fix: include random sample header

--- include/infini_operators.h
+++ include/infini_operators.h
@@ -4,6 +4,7 @@
 #include "ops/causal_softmax/causal_softmax.h"
 #include "ops/matmul/matmul.h"
 #include "ops/mlp/mlp.h"
+#include "ops/random_sample/random_sample.h"
 #include "ops/rearrange/rearrange.h"
 #include "ops/rms_norm/rms_norm.h"
 #include "ops/rotary_embedding/rotary_embedding.h"
