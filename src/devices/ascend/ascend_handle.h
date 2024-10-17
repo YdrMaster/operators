56ee0c9494556653bd626767dcba9e9855f351ec
zhangyue <14568307+zhangyue207@user.noreply.gitee.com>
2024-10-17 14:42:07 +0800

    add handle

---
+++ src/devices/ascend/ascend_handle.h
@@ -0,0 +1,28 @@
+#ifndef ASCEND_HANDLE_H
+#define ASCEND_HANDLE_H
+
+#include "common_ascend.h"
+#include "device.h"
+#include "status.h"
+#include <acl/acl.h>
+#include <acl/acl_base.h>
+#include <acl/acl_rt.h>
+#include <aclnn/acl_meta.h>
+#include <memory>
+
+struct AscendContext {
+    Device device;
+    int device_id;
+};
+typedef struct AscendContext *AscendHandle_t;
+
+infiniopStatus_t createAscendHandle(AscendHandle_t *handle_ptr, int device_id);
+
+infiniopStatus_t deleteAscendHandle(AscendHandle_t handle_ptr);
+
+template<typename T>
+void use_aclnn(AscendHandle_t handle, T const &f) {
+    aclrtSetDevice(handle->device_id);
+}
+
+#endif
