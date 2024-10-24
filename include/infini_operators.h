bb5bd915a7a8f16b32cf590bc65b5af71f3372ee
lizimin <coollizimin@gmail.com>
2024-10-24 17:06:58 +0800

    Add relu to infini_operators.h

--- include/infini_operators.h
+++ include/infini_operators.h
@@ -1,6 +1,7 @@
 #include "ops/causal_softmax/causal_softmax.h"
 #include "ops/matmul/matmul.h"
 #include "ops/rearrange/rearrange.h"
+#include "ops/relu/relu.h"
 #include "ops/rms_norm/rms_norm.h"
 #include "ops/rotary_embedding/rotary_embedding.h"
 #include "ops/swiglu/swiglu.h"
