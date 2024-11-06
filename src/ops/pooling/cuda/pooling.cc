4ed062979093e8749f5e1644cfacd08feddde2c0
Zimin Li <coollizimin@gmail.com>
2024-11-06 21:25:19 +0800

    Fix merge issues

--- src/ops/pooling/cuda/pooling.cc
+++ src/ops/pooling/cuda/pooling.cc
@@ -152,17 +152,19 @@ infiniopStatus_t cudaCreatePoolingDescriptor(CudaHandle_t handle,
         };
         return STATUS_SUCCESS;
     }
+    return STATUS_SUCCESS;
+}
 
-    infiniopStatus_t cudaGetPoolingWorkspaceSize(PoolingCudaDescriptor_t desc, uint64_t * size) {
-        *size = 0;
-        return STATUS_SUCCESS;
-    }
+infiniopStatus_t cudaGetPoolingWorkspaceSize(PoolingCudaDescriptor_t desc, uint64_t *size) {
+    *size = 0;
+    return STATUS_SUCCESS;
+}
 
-    infiniopStatus_t cudaDestroyPoolingDescriptor(PoolingCudaDescriptor_t desc) {
-        checkCudnnError(cudnnDestroyTensorDescriptor(desc->x_desc));
-        checkCudnnError(cudnnDestroyTensorDescriptor(desc->y_desc));
-        checkCudnnError(cudnnDestroyPoolingDescriptor(desc->pool_desc));
-        desc->cudnn_handles_t = nullptr;
-        delete desc;
-        return STATUS_SUCCESS;
-    }
+infiniopStatus_t cudaDestroyPoolingDescriptor(PoolingCudaDescriptor_t desc) {
+    checkCudnnError(cudnnDestroyTensorDescriptor(desc->x_desc));
+    checkCudnnError(cudnnDestroyTensorDescriptor(desc->y_desc));
+    checkCudnnError(cudnnDestroyPoolingDescriptor(desc->pool_desc));
+    desc->cudnn_handles_t = nullptr;
+    delete desc;
+    return STATUS_SUCCESS;
+}
