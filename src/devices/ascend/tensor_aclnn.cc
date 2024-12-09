3d11a03d45ac4949cf693479e98171b642c304dd
zhangyue <14568307+zhangyue207@user.noreply.gitee.com>
2024-12-09 13:45:48 +0800

    delete old infer storage shape code

--- src/devices/ascend/tensor_aclnn.cc
+++ src/devices/ascend/tensor_aclnn.cc
@@ -50,59 +50,6 @@ infiniopStatus_t aclnnTensorDescriptor::setDescriptor(aclDataType dtype, const s
     return STATUS_SUCCESS;
 }
 
-// infiniopStatus_t aclnnTensorDescriptor::inferStorageShape(){
-//     auto shape = std::vector<int64_t>();
-//     auto strides = std::vector<int64_t>();
-//     for (uint64_t i = 0; i < this->ndim; ++i) {
-//         if (this->shape[i] > 1){
-//             shape.push_back(this->shape[i]);
-//             strides.push_back(this->strides[i]);
-//         }else if (this->shape[i] <= 0){
-//             return STATUS_BAD_TENSOR_SHAPE;
-//         }
-//     }
-
-//     this->storageNdim = shape.size();
-//     this->storageShape = std::vector<int64_t>(this->storageNdim, 1);
-//     std::vector<uint64_t> indices(this->storageNdim);
-//     for (int64_t i = 0; i < this->storageNdim; ++i) {
-//         indices[i] = i;
-//     }
-
-//     std::sort(indices.begin(), indices.end(), [&](uint64_t a, uint64_t b) {
-//         return strides[a] > strides[b];
-//     });
-//     auto bound = 0; // upper bound of non-zero-strided dimension
-//     for (int64_t i = 0; i < this->storageNdim; ++i) {
-//         // sort shape and strides by strides
-//         shape[i] = this->shape[indices[i]];
-//         strides[i] = this->strides[indices[i]];
-//         if (strides[i] >= 1){
-//             bound++;
-//         }else if (strides[i] < 0){
-//             // negative stride not supported
-//             return STATUS_BAD_TENSOR_STRIDES;
-//         }
-//     }
-//     // Treat the last non-zero-strided dimension as continuous
-//     // All trilling zero-strided dimensions are treated as 1
-//     shape[bound - 1] = shape[bound - 1] * strides[bound - 1];
-//     strides[bound - 1] = 1;
-//     int64_t carry = 1;
-//     for (int64_t i = bound - 1; i > 0; --i) {
-//         // Each non-cummulative stride should be no smaller than corresponding dim
-//         // and storage shape is the bigger one
-//         this->storageShape[i] = strides[i-1] / carry;
-//         if (shape[i] > this->storageShape[i]){
-//                 return STATUS_BAD_TENSOR_STRIDES;
-//         }
-//         carry *= this->storageShape[i];
-//     }
-//     this->storageShape[0] = shape[0];
-
-//     return STATUS_SUCCESS;
-// }
-
 
 /// @brief Infer storage shape. For now this ruturns a 1D shape of the total tensor storage size.
 /// We don't see why higher dimensional storage shape is ever needed. To change if necesary.
